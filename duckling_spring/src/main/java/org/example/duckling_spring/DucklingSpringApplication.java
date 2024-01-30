06b6fd22fd70a0b00d81e6a2dd03141a4b2a834d
Johan Hultén <121808363+Zindriana@users.noreply.github.com>
2024-01-30 10:53:25 +0100

    Add files via upload

---
+++ duckling_spring/src/main/java/org/example/duckling_spring/DucklingSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.duckling_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucklingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DucklingSpringApplication.class, args);
+	}
+
+}
---
+++ duckling_spring/src/test/java/org/example/duckling_spring/DucklingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.duckling_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucklingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
